51ea1034fc8519078de4e6611e707d3123d8cba6
Muhammad Umair Nasir <umair.nasir014@gmail.com>
2023-02-07 07:20:48 +0200

    better gpt3

--- gpt3/evaluate.py
+++ gpt3/evaluate.py
@@ -1,8 +1,15 @@
 import os
 import pandas as pd
+import numpy as np
+from Levenshtein import distance
 
 from helpers import h5_to_df
 from sokoban_solvers import EnhancedAStarAgent, State
+
+
+
+
+
 
 
 import openai
@@ -10,16 +17,25 @@ os.environ['OPENAI_API_KEY'] = "sk-I321ZJVEoaHUVIyV02PhT3BlbkFJfEEk7lg5vBRhpeEqe
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-MICROBAN_PATH = "microban_data.h5"
+TRAIN_PATH = "levels.csv"
 
-def training_levels(path):
-    df = h5_to_df(path)
+def training_levels(path, is_h5=True):
+    """
+    Function to get training levels
+    """
+    if is_h5:
+        df = h5_to_df(path)
+    else:
+        df = pd.read_csv(path)
     return list(df.level)
     
 
 
 def is_unique(level):
-    return level not in training_levels(MICROBAN_PATH)
+    """
+    Function to calculate diversity
+    """
+    return level not in training_levels(TRAIN_PATH,is_h5=False)
 
 
 def is_playable(level, verbose=False):
@@ -35,6 +51,7 @@ def is_playable(level, verbose=False):
         solver = EnhancedAStarAgent()
         # Check if the level is rectangular
         line_lengths = [len(line) for line in level.split("\n")]
+        #print(set(line_lengths))
         if len(set(line_lengths)) != 1:
             if verbose: print("--Level is not rectangular--")
             return False
@@ -57,28 +74,48 @@ def is_playable(level, verbose=False):
 
         # Check if the level can be solved by an ASTAR agent
         level_state = State().stringInitialize(level.split("\n"))
-        solution, node, iters = solver.getSolution(level_state, maxIterations=10000000000)
+        solution, node, iters = solver.getSolution(level_state, maxIterations=1000000000000000000000)
         if not node.checkWin():
             if verbose: print("--Level cannot be solved (... in 50k steps)--")
-            return False
+            #return True
         elif verbose:
             print(f"++Level can be solved in {len(solution)} moves++")
 
         return solution
 
-def eval(model,iterations):
+
+def is_novel(series, row_index):
+    """
+    Function to calculate novelty
+    """
+    distances = []
+    for i, value in enumerate(series):
+        if i != row_index:
+            d = distance(series[row_index], value)
+            if d < 5:
+                return False
+            
+    return True#np.mean(distances)
+
+
+def eval(model,simulations, model_name, exp_no):
 
     generations = {}
     generations["level"] = []
     generations["is_unique"] = []
+    generations["is_novel"] = []
     generations["is_playable"] = []
-    for iter in range(0, iterations):
+
+    temp = 0.55
+    top_p = 1
+    for sss in range(0, simulations):
 
         gen = openai.Completion.create(
                                        model=model,
-                                       prompt="Map. ->",
+                                       prompt="Map: ->",
                                        max_tokens=150,
-                                       temperature=0.5,
+                                       temperature=temp,
+                                       top_p = top_p,
                                        stop = [". END"]
                                       )
         
@@ -86,36 +123,69 @@ def eval(model,iterations):
         
         generations["level"].append(level)
         generations["is_unique"].append(is_unique(level))
-        generations["is_playable"].append(is_playable(level))
+        generations["is_playable"].append(is_playable(level, verbose = True))
+        generations["is_novel"].append(is_novel(generations["level"],len(generations["level"])-1))
+    
 
     df = pd.DataFrame(generations)
-    df.to_csv("result_5epoch.csv")
-    return df
+    is_p = df.loc[df.is_playable != False]# Playability
+    is_u = is_p.loc[is_p.is_unique != False]# Diversity
+    is_n = df.loc[df.is_novel != False]# novelty
+    
+    is_dpn = is_p.loc[is_p.is_novel != False] # Diversity of set of playable and novel levels
+
+
+    df.to_csv(f"exp_results/result_{model_name}_{temp}-temp_{top_p}-top_p_{exp_no}.csv")
+    return df, is_p.shape[0], is_u.shape[0], is_n.shape[0], is_dpn.shape[0]
         
         
 ### CHECKPOINTS
-
-model = {
+### MICROBAN
+model_1 = {
 "3_epochs" : "davinci:ft-gameinnovationlab:microban-sample-2-2023-02-05-20-39-06",
 "5_epochs" : "davinci:ft-gameinnovationlab:microban-sample-2-5epochs-2023-02-05-21-38-42",
+"7_epochs" : "davinci:ft-gameinnovationlab:microban-sample-2-7epochs-2023-02-06-04-12-28",
 "10_epochs" : "davinci:ft-gameinnovationlab:microban-sample-2-10epochs-2023-02-05-21-20-44",
+"15_epochs" : "davinci:ft-gameinnovationlab:microban-sample-2-15epochs-2023-02-06-07-16-30",
+"25_epochs" : "davinci:ft-gameinnovationlab:microban-sample-4-25epochs-2023-02-06-09-34-00"
 }
 
-eval(model["5_epochs"],30)
+model_2 = {
+    "5_epochs" : "davinci:ft-gameinnovationlab:microban-sample-3-3epochs-2023-02-06-07-40-13",
+    "15_epochs" : "davinci:ft-gameinnovationlab:microban-sample-3-10epochs-2023-02-06-08-23-25",
+}
+
+model_3 = {
+
+    "10_epochs" : "davinci:ft-gameinnovationlab:microban-sample-4-10epochs-2023-02-06-08-54-14"
+}
+
+model_4 = {
+    "5_epochs" : "curie:ft-gameinnovationlab:microban-sample-5-5epochs-2023-02-06-10-54-34"
+}
 
 
+### 600Level Boxoban
 
-#gens = openai.Completion.create(
-#  model="davinci:ft-gameinnovationlab:microban-sample-2-5epochs-2023-02-05-21-38-42",
-#  prompt="Map. ->",
-#  max_tokens=150,
-#  temperature=0.3,
-#  stop = [". END"]
-#)
+model_5 = {
+    "2_epochs" : "davinci:ft-gameinnovationlab:600level-sample-1-2epochs-2023-02-06-20-33-06"
+}
 
-#level = gens["choices"][0]["text"][1:]
-#print(is_unique(level))
-#print(is_playable(level,verbose=True))
-#print(level)
+model_6 = {
+    "2_epochs" : "davinci:ft-gameinnovationlab:600level-sample-1-2epochs-2023-02-06-21-02-50",
+    "3_epochs" : "davinci:ft-gameinnovationlab:600level-sample-1-3epochs-2023-02-06-21-48-09",
+    "6_epochs" : "davinci:ft-gameinnovationlab:600level-sample-1-4epochs-2023-02-06-21-26-43"
+}
 
-    
+model_7 = {
+    "1_epochs" : "davinci:ft-gameinnovationlab:600level-sample-2-1epochs-2023-02-06-22-11-00"
+}
+model_name = "davinci"
+
+exp_no = 16
+
+simulations = 100
+
+df, is_p, is_u, is_n, is_dqn = eval(model_6["6_epochs"],simulations,model_name,exp_no)
+
+print(f'Playability:{is_p/simulations}, Uniqueness: {is_u/simulations}, Novelty: {is_n/simulations}, Diversity of playable and novel levels: {is_dqn/simulations}')    
---
+++ gpt3/extract_boxoban.py
@@ -0,0 +1,89 @@
+import json
+import pandas as pd
+
+
+
+def extract_patterns_to_list(file_name):
+
+    """
+    A function that takes boxoban style data and converts it into List[levels: str].
+    Each str is a level.
+    """
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+
+    patterns = []
+    pattern = []
+    pattern_start = False
+    for line in lines:
+        if line.startswith(';'):
+            pattern_start = False
+            if pattern:
+                patterns.append(''.join(pattern))
+                pattern = []
+        if line == '##########\n':
+            pattern_start = True
+        if pattern_start:
+            pattern.append(line.replace(" ", "-"))
+
+    return patterns[:-2]
+
+def extract_pattern_info(pattern):
+    """
+    A function that takes in a str of level and returns a prompt.
+    """
+    lines = pattern.split('\n')
+    len_of_rows = len(lines[0])
+    no_of_rows = len(lines) -2 # Subtracting 2 '\n's at the end of last row. Case specific hard coding :/
+    #print(len_of_rows,no_of_rows)
+    no_of_base_tiles = pattern.count('#')
+    no_of_space_tiles = pattern.count('-')
+    no_of_dollar_tiles = pattern.count('$')
+    no_of_dot_tiles = pattern.count('.')
+    no_of_at_tiles = pattern.count('@')
+
+    return (
+        f'The size of the level is {len_of_rows} x {no_of_rows}. There is a base tile "#" with the count {no_of_base_tiles} and a space tile " " with the count {no_of_space_tiles}. There are three fixed number tiles: "$", "@" and ".". The count of "$" is {no_of_dollar_tiles}, "." is {no_of_dot_tiles}, and "@" is {no_of_at_tiles}.'
+    )
+
+def extract_patterns_info(file_name):
+    """
+    A function that takes List[str] of levels and returns List[prompts: str]
+    """
+    patterns = extract_patterns_to_list(file_name)
+    return [extract_pattern_info(pattern) for pattern in patterns]
+
+
+def create_data(prompts, completions, prompt_type):
+    """
+    A function that takes in prompts and completions and generates a JSONL file
+    that is required to fine-tune GPT-3.
+    """
+    data = []
+    if prompt_type == "parser":
+        for i, parser in enumerate(prompts):
+            prompt = {"prompt": f"{parser}->", "completion": f" {completions[i]}. END"}
+            data.append(prompt)
+
+    elif prompt_type == "sampler":
+        for i, parser in enumerate(prompts):
+            prompt = {"prompt": "Map: ->", "completion": f" {completions[i][:-2]}. END"}
+            data.append(prompt)
+
+    with open("data_000.jsonl", "w") as f:
+        for item in data:
+            f.write(json.dumps(item) + "\n")
+
+
+def get_data(file):
+    prompts = extract_patterns_info(file)
+    completions = extract_patterns_to_list(file)
+    data = create_data(prompts, completions,prompt_type="sampler")
+    df = pd.DataFrame({'level':completions})
+    df.to_csv("levels.csv")
+
+
+
+
+
+get_data("data_000.txt")
